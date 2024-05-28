43d1217186e5ea1e9ca008ac2fbf0d1f0ae2f1a3
Milan-Bhingradiya <milanbhingradiya00@gmail.com>
2024-05-28 22:12:42 +0530

    solve dynamic route naming

--- src/app/api/utility/dbConnect.ts
+++ src/app/api/utility/dbConnect.ts
@@ -1,15 +1,16 @@
 // lib/dbConnect.ts
+// this one i use....
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-console.log(MONGODB_URI)
-console.log(MONGODB_URI)
-console.log(MONGODB_URI)
-console.log(MONGODB_URI)
-console.log(MONGODB_URI)
-console.log(MONGODB_URI)
-console.log(MONGODB_URI)
+// console.log(MONGODB_URI)
+// console.log(MONGODB_URI)
+// console.log(MONGODB_URI)
+// console.log(MONGODB_URI)
+// console.log(MONGODB_URI)
+// console.log(MONGODB_URI)
+// console.log(MONGODB_URI)
 if (!MONGODB_URI) {
     
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
